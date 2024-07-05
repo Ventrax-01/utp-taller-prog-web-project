ff677c6d6d82bf60a62e34af402e0ca514007a3a
Alonso Giraldo <alonso.giraldo@inversiones.io>
2024-07-05 18:52:20 -0500

    changes

--- modules/student/index.php
+++ modules/student/index.php
@@ -1,11 +1,9 @@
 <?php
 session_start();
-echo "TEST";
 if (!isset($_SESSION['alumno_id'])) {
     echo "<script> location.href='login.php'; </script>";
     exit();
 }
-echo "TEST2";
 
 require_once 'controllerAlumno.php';
 $controller = new ControllerAlumno();
