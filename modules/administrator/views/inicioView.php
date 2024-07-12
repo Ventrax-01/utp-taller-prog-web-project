ceb3c6e8d40c5d143a3489eb48ff84fb1fa361c8
Ventrax <alonsolgm001@gmail.com>
2024-07-12 00:20:40 -0500

    Testing stuff

---
+++ modules/administrator/views/inicioView.php
@@ -0,0 +1,124 @@
+<div class="col-12 col-lg-4">
+    <div
+        class="cell bg-danger bg-opacity-10 border border-danger rounded h-100"
+    >
+        <div class="m-3">
+            <h3 class="module-tag-header">
+                Hola, Alonso Giraldo
+            </h3>
+            <span
+                >Bienvenido al panel de administrador. Aquí
+                tendrás la capacidad de supervisar y ajustar
+                diversos aspectos del sistema escolar según las
+                necesidades de la institución.
+            </span>
+        </div>
+    </div>
+</div>
+<div class="col-12 col-lg-8">
+    <div
+        class="cell bg-danger bg-opacity-10 border border-danger rounded h-100"
+    >
+        <div class="m-3">
+            <h3 class="module-tag-header">
+                Últimas conexiones
+            </h3>
+            <table
+                class="table table-striped table-bordered border-danger small"
+            >
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Persona</th>
+                        <th scope="col">Cargo</th>
+                        <th scope="col">Fecha y hora</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<div class="col-12 col-lg-4">
+    <div
+        class="cell bg-danger bg-opacity-10 border border-danger rounded"
+    >
+        <div class="m-3">
+            <h3 class="module-tag-header">Cursos</h3>
+            <span
+                >Lorem ipsum dolor sit, amet consectetur
+                adipisicing elit. Sapiente laborum ipsum hic
+                itaque, obcaecati aliquid nam optio, eum
+                consequatur nostrum atque, autem alias harum
+                pariatur maiores quos! Ad, itaque
+                cupiditate.</span
+            >
+        </div>
+    </div>
+</div>
+<div class="col-12 col-lg-4">
+    <div
+        class="cell bg-danger bg-opacity-10 border border-danger rounded"
+    >
+        <div class="m-3">
+            <h3 class="module-tag-header">Grados</h3>
+            <span
+                >Lorem ipsum dolor sit, amet consectetur
+                adipisicing elit. Sapiente laborum ipsum hic
+                itaque, obcaecati aliquid nam optio, eum
+                consequatur nostrum atque, autem alias harum
+                pariatur maiores quos! Ad, itaque
+                cupiditate.</span
+            >
+        </div>
+    </div>
+</div>
+<div class="col-12 col-lg-4">
+    <div
+        class="cell bg-danger bg-opacity-10 border border-danger rounded"
+    >
+        <div class="m-3">
+            <h3 class="module-tag-header">Finanzas</h3>
+            <span
+                >Lorem ipsum dolor sit, amet consectetur
+                adipisicing elit. Sapiente laborum ipsum hic
+                itaque, obcaecati aliquid nam optio, eum
+                consequatur nostrum atque, autem alias harum
+                pariatur maiores quos! Ad, itaque
+                cupiditate.</span
+            >
+        </div>
+    </div>
+</div>
