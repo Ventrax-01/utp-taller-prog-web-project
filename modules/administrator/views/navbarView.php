8caeec42a185421696a7a04e2dd022128d21dc60
Ventrax <alonsolgm001@gmail.com>
2024-07-12 00:07:31 -0500

    Testing stuff

---
+++ modules/administrator/views/navbarView.php
@@ -0,0 +1,43 @@
+<button
+    class="navbar-toggler custom-toggler"
+    type="button"
+    data-bs-toggle="offcanvas"
+    data-bs-target="#offcanvasNavbarDark"
+    aria-controls="offcanvasNavbarDark"
+>
+    <span class="navbar-toggler-icon"></span>
+</button>
+<div>
+    <div>
+        <div class="col-12 d-flex justify-content-end">
+            <div class="container text-center">
+                <div class="row">
+                    <div
+                        class="col container text-middle navbar-data-text"
+                    >
+                        <div class="row">
+                            <span
+                                >Hola,
+                                <b>Alonso Giraldo</b></span
+                            >
+                        </div>
+                        <div class="row email">
+                            <span>u23232870@utp.edu.pe</span>
+                        </div>
+                    </div>
+                    <div class="col d-flex align-items-center">
+                        <div class="exit-logo-div">
+                            <img
+                                id="exit-logo"
+                                src="../../assets/images/logout_logo.png"
+                                alt="logout"
+                                width="35px"
+                                height="35px"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ modules/administrator/views/sidebarView.php
@@ -0,0 +1,104 @@
+<div
+    class="offcanvas offcanvas-start custom-offcanvas"
+    tabindex="-1"
+    id="offcanvasNavbarDark"
+    aria-labelledby="offcanvasNavbarDarkLabel"
+>
+    <div class="offcanvas-header">
+        <h5
+            class="offcanvas-title"
+            id="offcanvasNavbarDarkLabel"
+        >
+            ADMINISTRACIÓN
+        </h5>
+        <button
+            type="button"
+            class="btn-close btn-close-white"
+            data-bs-dismiss="offcanvas"
+            aria-label="Close"
+        ></button>
+    </div>
+    <div class="offcanvas-body">
+        <ul
+            class="navbar-nav justify-content-end flex-grow-1 pe-4"
+        >
+            <li class="nav-item">
+                <a class="nav-link offcanvas-nav-link" href="/modules/administrator/"
+                    ><i
+                        class="fa-solid fa-shapes fa-offcanvas-logos fa-fw"
+                    ></i
+                    ><span
+                        class="d-inline-block fa-offcanvas-text"
+                        >Inicio</span
+                    ></a
+                >
+            </li>
+            <li class="nav-item">
+                <a
+                    class="nav-link d-inline-block offcanvas-nav-link"
+                    href="alumnos.html"
+                >
+                    <i
+                        class="fa-solid fa-user-graduate fa-offcanvas-logos fa-fw"
+                    ></i
+                    ><span
+                        class="d-inline-block fa-offcanvas-text"
+                        >Alumnos</span
+                    ></a
+                >
+            </li>
+            <li class="nav-item">
+                <a
+                    class="nav-link d-inline-block offcanvas-nav-link"
+                    href="#"
+                    ><i
+                        class="fa-solid fa-person-chalkboard fa-offcanvas-logos fa-fw"
+                    ></i
+                    ><span
+                        class="d-inline-block fa-offcanvas-text"
+                        >Profesores</span
+                    ></a
+                >
+            </li>
+            <li class="nav-item">
+                <a
+                    class="nav-link d-inline-block offcanvas-nav-link"
+                    href="#"
+                    ><i
+                        class="fa-solid fa-book-open fa-offcanvas-logos fa-fw"
+                    ></i
+                    ><span
+                        class="d-inline-block fa-offcanvas-text"
+                        >Cursos</span
+                    ></a
+                >
+            </li>
+            <li class="nav-item">
+                <a
+                    class="nav-link d-inline-block offcanvas-nav-link"
+                    href="#"
+                    ><i
+                        class="fa-solid fa-school fa-offcanvas-logos fa-fw"
+                    ></i
+                    ><span
+                        class="d-inline-block fa-offcanvas-text"
+                        >Grados</span
+                    ></a
+                >
+            </li>
+            <li class="nav-item">
+                <a
+                    class="nav-link d-inline-block offcanvas-nav-link"
+                    href="#"
+                    ><i
+                        class="fa-solid fa-dollar-sign fa-offcanvas-logos fa-fw"
+                    ></i
+                    ><span
+                        class="d-inline-block fa-offcanvas-text"
+                        >Finanzas</span
+                    ></a
+                >
+            </li>
+        </ul>
+    </div>
+</div>
